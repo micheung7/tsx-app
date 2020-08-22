2d44bef435b61b5a99392be213123225c7b83d0d
Michelle Cheung <micheung@berkeley.edu>
2020-08-21 23:26:11 -0700

    Updates

---
+++ precache-manifest.7ad21d492e791cc2ed12086dada520fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124d5397d9d25d3468e32e8e7838c0a2",
+    "url": "/tsx-app/index.html"
+  },
+  {
+    "revision": "2b9e76f5827654a3eff1",
+    "url": "/tsx-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2192cd6f1ed22da0e26f",
+    "url": "/tsx-app/static/js/2.75b26b0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tsx-app/static/js/2.75b26b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b9e76f5827654a3eff1",
+    "url": "/tsx-app/static/js/main.6ef2af9b.chunk.js"
+  },
+  {
+    "revision": "cec481a484d230ced582",
+    "url": "/tsx-app/static/js/runtime-main.a423f21a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tsx-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
