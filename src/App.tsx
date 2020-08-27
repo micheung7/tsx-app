f993d0460b395558c959dec25f0e9fbc128c9e44
Michelle Cheung <micheung@berkeley.edu>
2020-08-27 01:05:41 -0700

    Add icon buttons with links. Make grid list adjust to changes in page size. Style title bars.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import tileData from './images/tileData';
+import Avatar from '@material-ui/core/Avatar';
+
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
@@ -13,16 +15,27 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       display: 'flex',
       flexWrap: 'wrap',
-      justifyContent: 'space-around',
+      justifyContent: 'space-evenly',
       overflow: 'hidden',
       backgroundColor: theme.palette.background.paper,
+      margin: '25px',
     },
     gridList: {
-      width: 500,
-      height: 450,
+      justifyContent: 'space-evenly',
     },
-    icon: {
-      color: 'rgba(255, 255, 255, 0.54)',
+    titleBar: {
+      background: 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    title: {
+      color: '#a8fff9',
+    },
+    subtitle: {
+      color: '#cefc03',
+    },
+    avatarSize: {
+      width: '250px',
+      height: '250px',
+      margin: theme.spacing(.5),
     },
   }),
 );
@@ -33,36 +46,37 @@ function App() {
   return (
     <div className="App">
       <div className={classes.root}>
-        <GridList cellHeight={180} className={classes.gridList}>
+        <GridList className={classes.gridList} spacing={10}>
           <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-            <ListSubheader component="div">Site is a Work in Progress...</ListSubheader>
-            <img src={require('./images/code.svg')} alt="code" width="30" height="30"/>
+            <Fragment>
+              <ListSubheader component="div">Site is a Work in Progress...</ListSubheader>
+              <img src={require('./images/code.svg')} alt="code" width="30" height="30"/>
+            </Fragment>
           </GridListTile>
           {tileData.map((tile) => (
-            <GridListTile key={tile.img}>
-              <img src={tile.img} alt={tile.title} />
-              <GridListTileBar
-                title={tile.title}
-                // subtitle={<span>by: </span>}
-                actionIcon={
-                  <div
-                    // onClick={() => }
-                  >
-                  <IconButton
-                    aria-label={`info about ${tile.title}`}
-                    className={classes.icon}
-                  >
-                    <img src={require('./images/next-arrow.svg')} alt="next-arrow" width="30" height="30"/>
-                  </IconButton>
-                  </div>
-                }
-              />
-            </GridListTile>
+            <Fragment>
+              <GridListTile key={tile.img}>
+                <IconButton
+                  aria-label={`info about ${tile.title}`}
+                  href={tile.url}
+                >
+                  <Avatar alt={tile.title} src={tile.img} variant='circle' className={classes.avatarSize}/>
+                </IconButton>
+                <GridListTileBar
+                  classes={{
+                    root: classes.titleBar,
+                    title: classes.title,
+                    subtitle: classes.subtitle,
+                  }}
+                  title={tile.title}
+                  subtitle={<span>Description</span>}
+                />
+              </GridListTile>
+            </Fragment>
           ))}
         </GridList>
       </div>
       <ListSubheader component="div">Attributions</ListSubheader>
-      <div>Arrow Icon made by <a href="https://www.flaticon.com/free-icon/right_1251850" title="Icongeek26">Icongeek26</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       <div>Code Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </div>
   );
--- src/images/tileData.tsx
+++ src/images/tileData.tsx
@@ -2,26 +2,32 @@ const tileData = [
   {
     img: require('./cat-bar.jpeg'),
     title: 'Cat Bar',
+    url: 'https://www.google.com/chrome/',
   },
   {
     img: require('./cat-bathtub.jpeg'),
     title: 'Cat Bathtub',
+    url: 'https://www.mozilla.org/en-US/firefox/new/',
   },
   {
     img: require('./cat-dashboard.jpeg'),
     title: 'Cat Dashboard',
+    url: 'https://support.apple.com/downloads/safari',
   },
   {
     img: require('./cat-lap.jpeg'),
     title: 'Cat Lap',
+    url: 'https://www.microsoft.com/en-us/edge',
   },
   {
     img: require('./cat-tongue.jpeg'),
     title: 'Cat Tongue',
+    url: 'https://support.microsoft.com/en-us/help/17621/internet-explorer-downloads',
   },
   {
     img: require('./cat-upsidedown.jpeg'),
     title: 'Cat Upsidedown',
+    url: 'https://www.opera.com/download',
   }
 ];
 
