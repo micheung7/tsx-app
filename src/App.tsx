0da01c620abcbcd977c6ec88eb3c675327fec0d1
Michelle Cheung <micheung@berkeley.edu>
2020-09-04 02:50:10 -0700

    Fix button link to use router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
 import Error from './components/Error';
@@ -7,7 +7,7 @@ import Home from './components/Home';
 import Learn from './components/Learn';
 import Navigation from './components/Navigation';
 import ListSubheader from '@material-ui/core/ListSubheader';
-import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
@@ -22,13 +22,13 @@ function App() {
               <Route component={Error}/>
             </Switch>
         </div> 
-      </BrowserRouter>
       <br></br>
-      <Button href='/tsx-app/learn' variant="outlined">
+      <Button component={NavLink} to="/tsx-app/learn" variant="outlined">
         <ListSubheader component="div">Site Development</ListSubheader>
         <img src={require('./images/code.svg')} alt="code" width="30" height="30"/>
         <ListSubheader component="div">...Work in Progress...</ListSubheader>
       </Button>
+      </BrowserRouter>
     </div>
   );
 }
