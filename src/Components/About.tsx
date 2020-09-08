00d592ef0f336f62c5dc63250ef6d1cf5a1174e9
Michelle Cheung <micheung@berkeley.edu>
2020-09-07 22:23:26 -0700

    Change About page layout and update content

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import SectionBox from './SectionBox';
+import { Button, createStyles, GridList, makeStyles, Theme } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    gridList: {
+      justifyContent: 'space-evenly',
+    },
+  }),
+);
 
 function About() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Hello! My name is Michelle.</p>
-      <p>I'm creating this personal website to learn more about web development.</p>
-      <p>Fun fact: My original goal for this project was just to learn how to deploy a site using GitHub Pages.</p>
-      <p>However, I've learned a lot more since then!</p>
-      <i>Click <a href='/tsx-app/learn'>here</a> to see my progress updates.</i>
-    </div>
+    <GridList className={classes.gridList}>
+      <SectionBox content={<div>
+        <h1>About Us</h1>
+        <p>Hello! My name is Michelle.</p>
+        <p>I'm creating this personal website to learn more about web development.</p>
+        <p>Fun fact: My original goal for this project was just to learn how to deploy a site using GitHub Pages.</p>
+        <p>However, I've learned a lot more since then!</p>
+        <i>See<Button component={NavLink} to="/learn">Learn</Button>for progress updates.</i>
+      </div>}/>
+      <SectionBox content={<div>
+        <h1>Site Goals</h1>
+        <p>Create a React TypeScript app without outside expectaions or deadlines (hobby, not work).</p>
+        <p>Incremental research: Experiment with different libraries and frameworks for React.</p>
+        <p>Incremental site development: Add something new or interesting to the site, hopefully a few times a week.</p>
+        <p>Improve user experience: Learn about UI/UX and create mockups for the site.</p>
+        <p>Brainstorm more content for the site, outside of the typical personal bio and resume.</p>
+      </div>}/>
+    </GridList>
   );
 }
 
